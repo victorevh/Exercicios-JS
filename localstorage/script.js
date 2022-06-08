22933849ca6c7987d621e7703ffaf44101c9759f
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-06-08 00:50:36 -0300

    Refatora script localstorage

--- localstorage/script.js
+++ localstorage/script.js
@@ -1,41 +1,69 @@
+// Variaveis
+
 let favorites = JSON.parse(localStorage.getItem('favorites')) || []
-// pegar imagem externa
+const imageContainer = document.querySelector('.image')
+const button = document.querySelector('button')
 
-const getExternalImage = async () => {
-   const response = await fetch('https://source.unsplash.com/random')
+//Eventos
 
-   document.querySelector('.image')
-   .innerHTML = `<img src="${response.url}" >`
-}
+button.onclick = () => updateImage()
+imageContainer.onclick = () => updateAll()
 
-getExternalImage()
-
-// clicar no botão, pegar imagem externa
-
-document.querySelector('button').onclick = () => {
-    getExternalImage()
-}
-
-// clicar na imagem
-
-document.querySelector('.image').onclick = () => {
-    const imageContainer = document.querySelector('.image')
-
+// Methods
+const getState = () => {
     const imageSource = document.querySelector('.image img').src
-
-    // verificar se está no localstorage, remover.
 
     const index = favorites.indexOf(imageSource)
     const existsInLocalStorage = index != -1
-    if(existsInLocalStorage) {
+
+    return { imageSource, index, existsInLocalStorage}
+}
+
+const updateAll = () => {
+
+    updateFavorites()
+    updateClasses()
+    
+}
+
+const updateFavorites = () => {
+    const { existsInLocalStorage, index, imageSource } = getState()
+
+    existsInLocalStorage
+    //if
+    ? favorites.splice(index, 1)
+    //else
+    : favorites.push(imageSource)
+
+/*    if(existsInLocalStorage) {
         favorites.splice(index, 1)
-        imageContainer.classList.remove('fav')
     } else {    // salvar ou remover no localstorage
         favorites.push(imageSource)
-        imageContainer.classList.add('fav')
     }
-
+*/
+    
     localStorage.setItem('favorites', JSON.stringify(favorites))
 }
 
-// salvar no local storage ou remover
+const updateClasses = () => {
+    const { existsInLocalStorage} = getState()
+    imageContainer.classList.remove('fav')
+
+    if(existsInLocalStorage) {
+        imageContainer.classList.add('fav')
+    }
+}
+
+const updateImage = async () => {
+    await getExternalImage()
+    updateClasses()
+}
+
+const getExternalImage = async () => {
+    const response = await fetch('https://source.unsplash.com/random')
+ 
+    imageContainer
+    .innerHTML = `<img src="${response.url}" >`
+ }
+ 
+ getExternalImage()
