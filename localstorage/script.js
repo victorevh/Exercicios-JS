24f436260787aed51a7c3fefb7a8532d4fea36d1
Rubem Vasconcelos <rubemfsv15@gmail.com>
2022-10-25 11:16:45 -0300

    rafactor: adjust js indentation due to js pattern

--- localstorage/script.js
+++ localstorage/script.js
@@ -1,71 +1,67 @@
 // Variaveis
 
-let favorites = JSON.parse(localStorage.getItem('favorites')) || []
-const imageContainer = document.querySelector('.image')
-const button = document.querySelector('button')
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+const imageContainer = document.querySelector('.image');
+const button = document.querySelector('button');
 
 //Eventos
 
-button.onclick = () => updateImage()
-imageContainer.onclick = () => updateAll()
+button.onclick = () => updateImage();
+imageContainer.onclick = () => updateAll();
 
 // Methods
 const getState = () => {
-    const imageSource = document.querySelector('.image img').src
+  const imageSource = document.querySelector('.image img').src;
 
-    const index = favorites.indexOf(imageSource)
-    const existsInLocalStorage = index != -1
+  const index = favorites.indexOf(imageSource);
+  const existsInLocalStorage = index != -1;
 
-    return { imageSource, index, existsInLocalStorage}
-}
-
+  return { imageSource, index, existsInLocalStorage };
+};
 
 const updateFavorites = () => {
-    const { existsInLocalStorage, index, imageSource } = getState()
+  const { existsInLocalStorage, index, imageSource } = getState();
 
-    existsInLocalStorage
-    //if
-    ? favorites.splice(index, 1)
-    //else
-    : favorites.push(imageSource)
+  existsInLocalStorage
+    ? //if
+      favorites.splice(index, 1)
+    : //else
+      favorites.push(imageSource);
 
-// condicional mais legivel
-/*    if(existsInLocalStorage) {
+  // condicional mais legivel
+  /*    if(existsInLocalStorage) {
         favorites.splice(index, 1)
     } else {    // salvar ou remover no localstorage
         favorites.push(imageSource)
     }
 */
-    
-    localStorage.setItem('favorites', JSON.stringify(favorites))
-}
+
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+};
 
 const updateClasses = () => {
-    const { existsInLocalStorage} = getState()
-    imageContainer.classList.remove('fav')
+  const { existsInLocalStorage } = getState();
+  imageContainer.classList.remove('fav');
 
-    if(existsInLocalStorage) {
-        imageContainer.classList.add('fav')
-    }
-}
+  if (existsInLocalStorage) {
+    imageContainer.classList.add('fav');
+  }
+};
 
 const updateAll = () => {
-
-    updateFavorites()
-    updateClasses()
-    
-}
+  updateFavorites();
+  updateClasses();
+};
 
 const updateImage = async () => {
-    await getExternalImage()
-    updateClasses()
-}
+  await getExternalImage();
+  updateClasses();
+};
 
 const getExternalImage = async () => {
-    const response = await fetch('https://source.unsplash.com/random')
- 
-    imageContainer
-    .innerHTML = `<img src="${response.url}" >`
- }
- 
- getExternalImage()
+  const response = await fetch('https://source.unsplash.com/random');
+
+  imageContainer.innerHTML = `<img src="${response.url}" >`;
+};
+
+getExternalImage();
